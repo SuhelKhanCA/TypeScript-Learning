17f329da06cc1ee8f2efec04f760578c2bf07cfb
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 23:09:37 +0530

    tsc --target es2015 hello.ts

--- module2/03.js
+++ module2/03.js
@@ -1,6 +1,6 @@
 // Adding types
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 // greet("Maddison", Date()); // error TS2345: Argument of type 'string' is not assignable to parameter of type 'Date'
 // expected 
