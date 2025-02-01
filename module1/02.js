ef13e1a3205516306b59f21e328ef6507911540b
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 22:28:39 +0530

    accessing unknown property of an object

---
+++ module1/02.js
@@ -0,0 +1,7 @@
+// Property access
+var user = {
+    name: "Daniel",
+    age: 26,
+};
+console.log(user.location); // returns undefined
+
---
+++ module1/02.ts
@@ -0,0 +1,10 @@
+// Property access
+
+const user = {
+  name: "Daniel",
+  age: 26,
+};
+console.log(user.location); // returns undefined
+
+
+export {}
