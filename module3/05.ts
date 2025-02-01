dc408283940ac3cdae025576d0b2cf5020912df5
suhelkhanca <suhel.hata@gmail.com>
2025-02-02 02:50:06 +0530

    type safety in array

---
+++ module3/05.ts
@@ -0,0 +1,25 @@
+// Types in : Array
+
+const superHeros: string[] = [];
+
+// const heroPower: number[] = []
+const heroPower: Array<number> = [];
+
+// Type Alias
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+// 2D array
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [142, 50, 221],
+];
+
+// putting values to these arrays
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
