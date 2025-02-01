c3301ec05ecca3c7bf205c983dc6f68b9760b4c8
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 23:22:08 +0530

    summary

---
+++ module2/05.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello World";
+var lower = greetings.toLowerCase();
+console.log(greetings);
+console.log(lower);
+// number
+var userId = 334455.3;
+var fixedVal = userId.toFixed();
+console.log(fixedVal);
+userId = "Hii";
+// boolean
+var isLoggedIn = false;
+console.log(userId);
+// any
+var superhero;
+function getHero() {
+    return 10;
+    //   return "Ironman";
+}
+superhero = getHero();
+console.log(superhero);
---
+++ module2/05.ts
@@ -0,0 +1,34 @@
+// Summary
+
+let greetings: string = "Hello World";
+
+const lower = greetings.toLowerCase();
+
+console.log(greetings);
+console.log(lower);
+
+// number
+
+let userId = 334455.3;
+
+const fixedVal = userId.toFixed();
+console.log(fixedVal);
+
+userId = "Hii";
+
+// boolean
+let isLoggedIn: boolean = false;
+console.log(userId);
+
+// any
+
+let superhero: string;
+
+function getHero() {
+//   return 10;
+  return "Ironman";
+}
+
+superhero = getHero();
+console.log(superhero);
+export {};
