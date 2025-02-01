68aecbf1d02c1718b86af1a58f78463cbd051a2e
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 21:58:17 +0530

    add : Intro to typescript 1

---
+++ module1/01.js
@@ -0,0 +1,15 @@
+"use strict";
+// Type safety and calling method
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello World!";
+var ans = message.toLowerCase();
+console.log(ans);
+// message(); // Error --> Not a function : Type 'String' has no call signatures.
+// Potential risk of static-typing
+function showPrb(msg) {
+  return msg.toLowerCase();
+}
+var str = showPrb("This Is Going to work");
+console.log(str);
+var num = showPrb(404);
+console.log(num);
---
+++ module1/01.ts
@@ -0,0 +1,20 @@
+// Type safety and calling method
+
+let message: string = "Hello World!";
+
+let ans = message.toLowerCase();
+console.log(ans);
+
+// message(); // Error --> Not a function : Type 'String' has no call signatures.
+
+// Potential risk of static-typing
+function showPrb(msg) {
+  return msg.toLowerCase();
+}
+
+let str = showPrb("This Is Going to work");
+console.log(str);
+let num = showPrb(404);
+console.log(num);
+
+export {};
