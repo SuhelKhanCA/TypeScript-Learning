8ecb9664b9e9b598fc801b2c28ea0e44e0873862
suhelkhanca <suhel.hata@gmail.com>
2025-02-02 02:18:29 +0530

    The primitives

---
+++ module3/01.ts
@@ -0,0 +1,14 @@
+// The Primitives
+
+// 1.) number
+// 2.) string
+// 3.) boolean
+
+let name: string = "Suhel";
+
+let age: number = 22;
+
+let isHero = true; // type is inferred itself also
+
+
+export {}
