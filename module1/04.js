2dfd3d88cae98002c1722c191133f9eada3accc9
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 22:33:48 +0530

    simple typo

---
+++ module1/04.js
@@ -0,0 +1,6 @@
+var announcement = "Hello World!";
+// How quickly can you spot the typos?
+announcement.toLocaleLowercase();
+announcement.toLocalLowerCase();
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
