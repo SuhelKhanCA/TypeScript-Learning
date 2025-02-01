d6cad5021d6620c11eb6ce5364848dd50968852d
suhelkhanca <suhel.hata@gmail.com>
2025-02-01 22:40:23 +0530

    uncalled function and basic logic errors

--- module1/04.ts
+++ module1/04.ts
@@ -1,8 +1,22 @@
 const announcement = "Hello World!";
 
-// How quickly can you spot the typos?
 announcement.toLocaleLowercase();
 announcement.toLocalLowerCase();
 
-// We probably meant to write this...
+// We probably meant to write this
 announcement.toLocaleLowerCase();
+
+// uncalled functions
+function flipCoin() {
+  // Meant to be Math.random()
+  return Math.random < 0.5;
+}
+
+// basic logic errors
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+  console.log("if block");
+} else if (value === "b") {
+  // Oops, unreachable
+  console.log("else block");
+}
