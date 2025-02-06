7378ef0fcfe5224a75f19764622a47fc4b317d49
suhelkhanca <suhel.hata@gmail.com>
2025-02-06 19:07:54 +0530

    Add union types and type narrowing

--- module4/01.ts
+++ module4/01.ts
@@ -1,1 +1,35 @@
 // UNION Types
+
+let score: number | string = 33
+score = 44
+score = "55"
+
+function printId(id: number | string) {
+//   console.log(id.toUpperCase()); Error
+// Property 'toUpperCase' does not exist on type 'string | number'.
+// Property 'toUpperCase' does not exist on type 'number'.
+}
+
+// TYPE NARROWING
+function printIdCorrected(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+
+// IF SAME PROPERTY --> No narrowing need
+// Return type is inferred as number[] | string
+function getFirstThree(x: number[] | string) {
+  return x.slice(0, 3);
+}
+
+
+// Note : Application
+let seatAllotment: "aisle" | "middle" | "window";
+
+seatAllotment = "aisle"
+// seatAllotment = "crew" // Not assignable
