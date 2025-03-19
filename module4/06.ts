0b0afee1a0495d4858959d71feb03abcce9029bd
suhelkhanca <suhel.hata@gmail.com>
2025-03-20 00:44:02 +0530

    add : source code

---
+++ module4/06.ts
@@ -0,0 +1,6 @@
+// Type Assertions
+// type of a value that TypeScript can’t know about
+
+const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
+
+// const x = "hello" as number; // Error
